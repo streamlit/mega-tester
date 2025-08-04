d343741a41f27506aa2a7dd6e9d8e1244fc02d53
Johannes Rieke <johannes.rieke@snowflake.com>
2025-08-04 20:37:59 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,7 @@ import plotly.express as px
 import pydeck as pdk
 import streamlit as st
 
+LOREM = """Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."""
 
 # Add disable toggle in sidebar
 with st.sidebar:
@@ -550,6 +551,7 @@ large_dialog = st.toggle("Large dialog", False)
 @st.dialog("Test dialog", width="large" if large_dialog else "small")
 def dialog():
     st.write("Hello there!")
+    st.write(LOREM)
     if st.button("Close", disabled=disabled):
         st.rerun()
 
