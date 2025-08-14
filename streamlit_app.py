86790f9d54dbb553ebc8f664d6cac5e72c86ce73
Johannes Rieke <johannes.rieke@snowflake.com>
2025-08-15 01:12:50 +0200

    Add PDF

--- streamlit_app.py
+++ streamlit_app.py
@@ -529,6 +529,9 @@ st.video(
     "https://file-examples.com/storage/fe8d06553067e519a994eaa/2017/04/file_example_MP4_480_1_5MG.mp4"
 )
 
+"st.pdf"
+st.pdf("https://file-examples.com/wp-content/storage/2017/10/file-sample_150kB.pdf")
+
 
 "## Layouts and containers"
 
